4ae3842e0a47f2f8f8b2a3754d5b9ec931979a15
Joso1207 <joso1207@student.miun.se>
2025-12-18 15:54:24 +0100

    Update balance test passes

--- src/main/java/org/example/repository/AccountRepository.java
+++ src/main/java/org/example/repository/AccountRepository.java
@@ -2,12 +2,18 @@ package org.example.repository;
 
 import org.example.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
 
-    //@Query()
-    //void updateBalanceOnAccountWithID(Long id, BigDecimal newBalance);
+    @Modifying
+    @Query("UPDATE Account SET balance = ?2 WHERE id = ?1")
+    void updateBalanceOnAccountWithID(Long id,BigDecimal balance);
 
 }
